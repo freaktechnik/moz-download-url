2fdcd34642770cf904c9832b35cbeb1c79f67e9d
Martin Giger <martin@humanoids.be>
2016-08-09 14:02:58 +0200

    More tests on the constants anatomy

--- test/index.js
+++ test/index.js
@@ -1,31 +1,24 @@
 import test from 'ava';
 import mdu from '../';
 
+const TOP_LEVEL_CONSTS = [ "PLATFORMS", "FIREFOX", "FIREFOX_FOR_ANDROID", "THUNDERBIRD", "PRODUCTS" ];
 
 function checkConstName(t, constName) {
-    t.is(constName, constName.toUpperCase());
-    t.false(constName.includes("-"));
+    t.regex(constName, /^[A-Z0-9_]+$/);
 }
 checkConstName.title = (providedTitle, constName) => `Checking for correct naming of constant ${providedTitle}.${constName}`;
 
-for(let c in mdu.PLATFORMS) {
-    test("PLATFORMS", checkConstName, c);
+function checkConstValue(t, constValue) {
+    t.is(typeof constValue, "string");
 }
+checkConstValue.title = (providedTitle) => `Checking value of constant ${providedTitle}`;
 
-for(let c in mdu.FIREFOX) {
-    test("FIREFOX", checkConstName, c);
-}
-
-for(let c in mdu.FIREFOX_FOR_ANDROID) {
-    test("FIREFOX_FOR_ANDROID", checkConstName, c);
-}
-
-for(let c in mdu.THUNDERBIRD) {
-    test("THUNDERBIRD", checkConstName, c);
-}
-
-for(let c in mdu.PRODUCTS) {
-    test("PRODUCTS", checkConstName, c);
+for(const n of TOP_LEVEL_CONSTS) {
+    test(checkConstName, n);
+    for(const c in mdu[n]) {
+        test(n, checkConstName, c);
+        test(`${n}.${c}`, checkConstValue, mdu[n][c]);
+    }
 }
 
 test("Check basic module anatomy", (t) => {
@@ -41,6 +34,19 @@ test("Check basic module anatomy", (t) => {
 
     for(let p in expectedProperties) {
         t.is(typeof mdu[p], expectedProperties[p]);
+    }
+});
+
+test("All const names in PRODUCTS should also be top-level consts", (t) => {
+    for(const c in mdu.PRODUCTS) {
+        t.true(c in mdu);
+    }
+});
+
+test("Products should at least include LATEST and LATEST_BETA", (t) => {
+    for(const c in mdu.PRODUCTS) {
+        t.true("LATEST" in mdu[c]);
+        t.true("LATEST_BETA" in mdu[c]);
     }
 });
 
